0fc61e76cc83afe7a13c75405b3334ef4e18040f
Fabio dorakiewicz alves <fabinho.dorakiewicz@gmail.com>
2025-10-24 18:33:48 -0300

    + ex 8, 9 e 10

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocidade legal é ${vel}`)
+
+if(vel > 59.99){
+    console.log('sua velocidade ultrapassou o limite de 60km/h. MULTADO! pobre.')
+}
---
+++ aula011/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+console.log(`Voce vive no pais ${pais}`)
+if(pais == 'Brasil'){
+console.log(`voce é brasilerio!`)
+}
+console.log(`voce não é brasilerio!`)
